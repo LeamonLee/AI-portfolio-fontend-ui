e7f2045b09ea3571aea9026693d689a8b7961c1e
Leamon.Lee <Leamon.Lee@advantech.com>
2021-04-06 18:30:01 +0800

    新增upload表單

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@
 function App() {
   return (
     <div>
-      {/* <img src="http://localhost:5001/webcam" className="App-logo" alt="logo" /> */}
-      <img src="http://localhost:5001/image" className="App-logo" alt="logo" />
+      {/* <img src="http://localhost:5001/webcam" alt="logo" /> */}
+      {/* <img src="http://localhost:5001/image" alt="logo" /> */}
+      {/* <img src="" alt="logo" /> */}
       
+      <h4>Upload your image</h4>
+      <form action="http://localhost:5001/od/image_detect" method="POST" enctype="multipart/form-data">
+          <label for="">Select image</label>
+          <div>
+              <input type="file" name="image" id="image" />
+          </div>
+          <button type="submit">Upload</button>
+      </form>
     </div>
   );
 }
